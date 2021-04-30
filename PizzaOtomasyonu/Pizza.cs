81ab767d68ddc67a9b339c2f3b28ae0b2e2922df
mikayil155 <mikayil155@outlook.com>
2021-04-30 16:12:21 +0300

    null elave ettim

---
+++ PizzaOtomasyonu/Pizza.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOtomasyonu
+{
+    class Pizza
+    {
+        public string Adi { get; set; }
+        public decimal Fiyati { get; set; }
+        public Ebat Ebati { get; set; }
+        public KenarTip KenarTipi { get; set; }
+        public List<string> Malzemeler { get; set; }
+
+        public decimal Tutar
+        {
+            get
+            {
+                decimal tutar = 0;
+                tutar = Fiyati * (decimal)Ebati.Carpan;
+                tutar += KenarTipi.EkFiyat;
+                return tutar;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Adi;
+        }
+
+    }
+}
